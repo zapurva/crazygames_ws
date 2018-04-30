4961700dce9fb0fa60b83c637041c1c79eb68def
Apurva Joshi <zapurva@gmail.com>
2018-04-30 20:51:56 +1000

    implemented prelim_hover before time_optimal

---
+++ build/crazyflie_ros/crazyflie_controller/CMakeFiles/controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/controller.dir/src/controller.cpp.o"
+  "/home/jan/apurva_joshi/crazygames_ws/devel/lib/crazyflie_controller/controller.pdb"
+  "/home/jan/apurva_joshi/crazygames_ws/devel/lib/crazyflie_controller/controller"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/crazyflie_ros/crazyflie_controller/CMakeFiles/time_optimal_controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/time_optimal_controller.dir/src/time_optimal_controller.cpp.o"
+  "/home/jan/apurva_joshi/crazygames_ws/devel/lib/crazyflie_controller/time_optimal_controller.pdb"
+  "/home/jan/apurva_joshi/crazygames_ws/devel/lib/crazyflie_controller/time_optimal_controller"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/time_optimal_controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
